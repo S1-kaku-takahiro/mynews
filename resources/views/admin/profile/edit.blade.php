9fd286dab0ae807a964aadb96cae1e287604270d
Takahiro Kaku <tkhrkk854755+techboost@gmail.com>
2021-02-15 23:46:08 +0000

    submit assignment of PHP/Laravel 12

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,10 +1,13 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>MyNews</title>
-  </head>
-  <body>
-    <p>プロフィール編集画面</p>
-  </body>
-</html>
+@extends('layouts.profile')
+
+@section('title', 'プロフィールの編集')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 mx-auto">
+        <h2>プロフィール編集画面</h2>
+      </div>
+    </div>
+  </div>
+@endsection
--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -36,7 +36,29 @@
             </ul>
             
             <ul class="navbar-nav ml-auto">
-              
+              {{-- ログインしていないときログイン画面のリンク表示 --}}
+              @guest
+                <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                
+              {{-- ログインしてたらユーザー名とログアウトボタンを表示 --}}
+              @else
+                <li class="nav-item dropdown">
+                  <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name }} <span class="caret"></span>
+                  </a>
+                  
+                  <div clasls="dropdown-menu" aria-labelleby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                      
+                      {{ __('Logout') }}
+                    </a>
+                    
+                    <form id="loggout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                      @csrf
+                    </form>
+                  </div>
+                </li>
+              @endguest
             </ul>
           </div>
         </div>
