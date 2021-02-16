3e02efab78ceac70851df77ce5166758f10156ac
Takahiro Kaku <tkhrkk854755+techboost@gmail.com>
2021-02-16 21:37:37 +0000

    resubmit assignment of PHP/Laravel 12

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -15,7 +15,30 @@ class NewsController extends Controller
     
     public function create(Request $request)
     {
-        // admin/news/createへリダイレクト
+        // Varidation
+        $this->validate($request, News::$rules);
+        
+        $news = new News;
+        $form = $request->all();
+        
+        // フォームから画像が送信されたら、保存、$news->image_path　に画像のパスを保存
+        if (isset($form['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $news->image_path = basename($path);
+        }else{
+            $news->image_path = null;
+        }
+        
+        // フォームから送信された_tokenを削除する
+        unset($form['_token']);
+        
+        // フォームから送信されてきたimageを削除
+        unset($form['image']);
+        
+        // データベースに保存
+        $news->fill($form);
+        $news->save();
+        
         return redirect('admin/news/create');
     }
     
---
+++ database/migrations/2021_02_16_045545_create_news_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');    // ニュースタイトル保存
+            $table->string('body');     // ニュース本文保存
+            $table->string('image_path')->nullable(); // 画像のパス
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
