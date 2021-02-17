9962c478f545e01a7e7ec1f57bb30cc4319523c3
Takahiro Kaku <tkhrkk854755+techboost@gmail.com>
2021-02-17 03:56:04 +0000

    submit assignment of PHP/Laravel 14

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -1,13 +1,12 @@
 <?php
-
 namespace App\Http\Controllers\Admin;
-
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\News;
+
 class NewsController extends Controller
 {
-    //
     public function add()
     {
         return view('admin.news.create');
@@ -15,31 +14,24 @@ class NewsController extends Controller
     
     public function create(Request $request)
     {
-        // Varidation
         $this->validate($request, News::$rules);
         
         $news = new News;
         $form = $request->all();
         
-        // フォームから画像が送信されたら、保存、$news->image_path　に画像のパスを保存
         if (isset($form['image'])) {
             $path = $request->file('image')->store('public/image');
             $news->image_path = basename($path);
-        }else{
+        } else {
             $news->image_path = null;
         }
         
-        // フォームから送信された_tokenを削除する
         unset($form['_token']);
-        
-        // フォームから送信されてきたimageを削除
         unset($form['image']);
         
-        // データベースに保存
         $news->fill($form);
         $news->save();
         
         return redirect('admin/news/create');
     }
-    
 }
