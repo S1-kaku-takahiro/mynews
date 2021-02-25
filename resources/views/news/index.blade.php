7ed1021112fa3e37e89f4f7b41a00f8cfefdfd38
Takahiro Kaku <tkhrkk854755+techboost@gmail.com>
2021-02-25 05:50:58 +0000

    php/Laravel Heroku

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <hr color="#c0c0c0">
-        <p>テストです</p>
+        <p>カリキュラム「PHP/Laravelコース - Herokuへのデプロイ」の課題1の確認用。</p>
         @if (!is_null($headline))
             <div class="row">
                 <div class="headline col-md-10 mx-auto">
